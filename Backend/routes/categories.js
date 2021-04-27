37a5b00cd544a6e1161acf9e8cc5e2d910be5854
Beast2502 <mehulsaxena45@gmail.com>
2021-04-28 01:18:59 +0530

    new changes in product

--- Backend/routes/categories.js
+++ Backend/routes/categories.js
@@ -8,8 +8,20 @@ router.get(`/`,async (req,res)=>{
     if(!categoryList){
         res.status(500).json({success: false})
     }
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 
+
+})
+
+
+router.get('/:id',async(req,res)=>{
+    const category = await Category.findById(req.params.id);
+
+    if(!category){
+        res.status(500).json({message: 'The category with the given ID was not there'})
+    }
+
+    res.status(200).send(category);
 
 })
 
@@ -31,6 +43,26 @@ router.post(`/`, async(req,res)=>{
     }
 })
 
+
+router.put('/:id', async(req,res)=>{
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name:req.body.name,
+            icon:req.body.icon,
+            color: req.body.color
+        },
+        {
+            new:true
+        }
+    )
+
+    if(!category)
+    return res.status(400).send('the category cannot be created!');
+
+    res.send(category);
+})
+
 // api/v1/categories/:id
 router.delete('/:id',(req,res)=>{
     Category.findByIdAndRemove(req.params.id).then(
@@ -40,7 +72,7 @@ router.delete('/:id',(req,res)=>{
             }
 
             else{
-                res.status(404).json({success: false, message:"Category not fount"})
+               return res.status(404).json({success: false, message:"Category not found"})
             }
         }).catch(err=>{
           return res.status(400).json({success: false, error: err})  
